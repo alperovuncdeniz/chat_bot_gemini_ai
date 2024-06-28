ef25dcd3c2da14425f7043efbe91df9263bdf7b6
Alper Övünç Deniz <alperovuncdeniz@gmail.com>
2024-06-28 21:21:12 +0300

    a

--- lib/repositories/chat_repository.dart
+++ lib/repositories/chat_repository.dart
@@ -1,3 +1,4 @@
+import 'package:chat_bot_gemini_ai/extensions/extensions.dart';
 import 'package:chat_bot_gemini_ai/models/message.dart';
 import 'package:chat_bot_gemini_ai/repositories/storage_repository.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -39,6 +40,105 @@ class ChatRepository {
       message = message.copyWith(
         imageUrl: downloadUrl,
       );
+    }
+
+    //Save message to Firebase
+    await _firestore
+        .collection("conversations")
+        .doc(userId)
+        .collection("messages")
+        .doc(sentMessageId)
+        .set(message.toMap());
+
+    GenerateContentResponse response;
+
+    try {
+      if (image == null) {
+        response = await textModel.generateContent([Content.text(promptText)]);
+      } else {
+        final imageBytes = await image.readAsBytes();
+
+        final promt = TextPart(promptText);
+
+        final mimeType = image.getMimeTypeFromExtension();
+        final imagePart = DataPart(mimeType, imageBytes);
+
+        response = await imageModel.generateContent([
+          Content.multi([
+            promt,
+            imagePart,
+          ])
+        ]);
+      }
+
+      final responseText = response.text;
+
+      final receivedMessageId = const Uuid().v4();
+
+      final responseMessage = Message(
+        id: receivedMessageId,
+        message: responseText!,
+        createdAt: DateTime.now(),
+        isMine: false,
+      );
+
+      await _firestore
+          .collection("conversations")
+          .doc(userId)
+          .collection("messages")
+          .doc(receivedMessageId)
+          .set(responseMessage.toMap());
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future sendTextMessage({
+    required String textPromt,
+    required String apiKey,
+  }) async {
+    try {
+      final textModel = GenerativeModel(model: "gemini-pro", apiKey: apiKey);
+
+      final userId = _auth.currentUser!.uid;
+      final sentMessageId = const Uuid().v4();
+
+      Message message = Message(
+        id: sentMessageId,
+        message: textPromt,
+        createdAt: DateTime.now(),
+        isMine: true,
+      );
+
+      await _firestore
+          .collection("conversations")
+          .doc(userId)
+          .collection("messages")
+          .doc(sentMessageId)
+          .set(message.toMap());
+
+      final response =
+          await textModel.generateContent([Content.text(textPromt)]);
+
+      final responseText = response.text;
+
+      final receivedMessageId = const Uuid().v4();
+
+      final responseMessage = Message(
+        id: receivedMessageId,
+        message: responseText!,
+        createdAt: DateTime.now(),
+        isMine: false,
+      );
+
+      await _firestore
+          .collection("conversations")
+          .doc(userId)
+          .collection("messages")
+          .doc(receivedMessageId)
+          .set(responseMessage.toMap());
+    } catch (e) {
+      throw Exception(e.toString());
     }
   }
 }
